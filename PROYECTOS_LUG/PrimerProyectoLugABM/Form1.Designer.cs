cb122e8e7eb393580aff0de833b145db133ce5fa
Marcos <iannellomarcos@gmail.com>
2023-08-23 23:17:56 -0300

    primer proyecto lug finalizado

---
+++ PROYECTOS_LUG/PrimerProyectoLugABM/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace ConnectionStringEjemplo
+{
+    partial class frmFacultad
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnDesconectar = new System.Windows.Forms.Button();
+            this.txtIdAlumno = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gridALumnos = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridALumnos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnAgregar.Location = new System.Drawing.Point(52, 196);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 32);
+            this.btnAgregar.TabIndex = 0;
+            this.btnAgregar.Text = "+";
+            this.btnAgregar.UseVisualStyleBackColor = false;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnDesconectar
+            // 
+            this.btnDesconectar.Location = new System.Drawing.Point(241, 282);
+            this.btnDesconectar.Name = "btnDesconectar";
+            this.btnDesconectar.Size = new System.Drawing.Size(120, 32);
+            this.btnDesconectar.TabIndex = 0;
+            this.btnDesconectar.Text = "Desconectar";
+            this.btnDesconectar.UseVisualStyleBackColor = true;
+            this.btnDesconectar.Click += new System.EventHandler(this.btnDesconectar_Click);
+            // 
+            // txtIdAlumno
+            // 
+            this.txtIdAlumno.Location = new System.Drawing.Point(151, 30);
+            this.txtIdAlumno.Name = "txtIdAlumno";
+            this.txtIdAlumno.Size = new System.Drawing.Size(210, 22);
+            this.txtIdAlumno.TabIndex = 1;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(151, 74);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(210, 22);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(151, 120);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(210, 22);
+            this.txtEdad.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnDesconectar);
+            this.groupBox1.Controls.Add(this.btnReset);
+            this.groupBox1.Controls.Add(this.btnEliminar);
+            this.groupBox1.Controls.Add(this.btnModificar);
+            this.groupBox1.Controls.Add(this.btnAgregar);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.txtEdad);
+            this.groupBox1.Controls.Add(this.txtIdAlumno);
+            this.groupBox1.Location = new System.Drawing.Point(27, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(393, 345);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Form Alumnos";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Edad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(52, 282);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(116, 32);
+            this.btnReset.TabIndex = 0;
+            this.btnReset.Text = "Resetear Form";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnEliminar.Location = new System.Drawing.Point(286, 196);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 32);
+            this.btnEliminar.TabIndex = 0;
+            this.btnEliminar.Text = "🗑️";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnModificar.Location = new System.Drawing.Point(167, 196);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 32);
+            this.btnModificar.TabIndex = 0;
+            this.btnModificar.Text = "✏️";
+            this.btnModificar.UseVisualStyleBackColor = false;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IdAlumno";
+            // 
+            // gridALumnos
+            // 
+            this.gridALumnos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridALumnos.Location = new System.Drawing.Point(443, 12);
+            this.gridALumnos.Name = "gridALumnos";
+            this.gridALumnos.RowHeadersWidth = 51;
+            this.gridALumnos.RowTemplate.Height = 24;
+            this.gridALumnos.Size = new System.Drawing.Size(480, 351);
+            this.gridALumnos.TabIndex = 3;
+            this.gridALumnos.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridALumnos_CellClick);
+            // 
+            // frmFacultad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(935, 450);
+            this.Controls.Add(this.gridALumnos);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmFacultad";
+            this.Text = "Facultad";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridALumnos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnDesconectar;
+        private System.Windows.Forms.TextBox txtIdAlumno;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView gridALumnos;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnEliminar;
+    }
+}
+
---
+++ PROYECTOS_LUG/PrimerProyectoLugABM/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ConnectionStringEjemplo
+{
+    public partial class frmFacultad : Form
+    {
+        public frmFacultad()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection cn;
+
+        public void Conectar()
+        {
+            cn = new SqlConnection();
+            cn.ConnectionString = "Data Source=MARCOS-ASUS;Initial Catalog=FACULTAD;Integrated Security=True";
+            //EN EL TEORICO TOMA QUE ES CADA COSA DEL CONNECTION STRING
+            cn.Open();
+        }
+
+        public void Desconectar()
+        {
+            cn.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CargarDataTable();
+        }
+
+        private void CargarDataTable()
+        {
+            gridALumnos.DataSource = null;
+
+            Conectar();
+
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = cn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "Select * from ALUMNOS";
+
+            DataTable TodosLosAlumnos = new DataTable();
+
+            TodosLosAlumnos.Load(cmd.ExecuteReader());
+
+            gridALumnos.DataSource = TodosLosAlumnos;
+
+            Desconectar();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Conectar();
+
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = cn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "Insert into ALUMNOS(idAlumno,Nombre,Edad) values("+int.Parse(txtIdAlumno.Text)+",'"+txtNombre.Text+"',"+ int.Parse(txtEdad.Text)+")";
+
+            cmd.ExecuteNonQuery();
+
+            Desconectar();
+
+            MessageBox.Show("SE AGREGO CORRECTAMENTE EL NUEVO REGISTRO");
+
+            clearControls();
+            CargarDataTable();
+            ResetearForm();
+        }
+
+        private void btnDesconectar_Click(object sender, EventArgs e)
+        {
+            Desconectar();
+        }
+
+        public void clearControls()
+        {
+            txtEdad.Clear();
+            txtIdAlumno.Clear();
+            txtNombre.Clear();
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            Conectar();
+
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = cn;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "UPDATE ALUMNOS SET Nombre = @Nombre, Edad = @Edad WHERE idAlumno = @idAlumno";
+
+            cmd.Parameters.AddWithValue("@Nombre", txtNombre.Text);
+            cmd.Parameters.AddWithValue("@Edad", int.Parse(txtEdad.Text));
+            cmd.Parameters.AddWithValue("@idAlumno", int.Parse(txtIdAlumno.Text));
+
+            cmd.ExecuteNonQuery();
+
+            Desconectar();
+
+            MessageBox.Show("Se modifico correctamente el alumno");
+            clearControls();
+            CargarDataTable();
+            ResetearForm();
+
+        }
+
+
+        private void gridALumnos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow row = gridALumnos.Rows[e.RowIndex];
+
+            string nombre = row.Cells["Nombre"].Value.ToString();
+            int edad = Convert.ToInt32(row.Cells["Edad"].Value);
+            int idAlumno = Convert.ToInt32(row.Cells["idAlumno"].Value);
+
+            txtNombre.Text = nombre;
+            txtIdAlumno.Text = idAlumno.ToString();
+            txtEdad.Text = edad.ToString();
+
+            txtIdAlumno.Enabled = false;
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            ResetearForm();
+        }
+
+        private void ResetearForm()
+        {
+            clearControls();
+            txtIdAlumno.Enabled = true;
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            Conectar();
+
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = cn;
+
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "Delete from Alumnos where idAlumno = @idAlumno";
+
+            cmd.Parameters.AddWithValue("@idAlumno", int.Parse(txtIdAlumno.Text));
+
+            cmd.ExecuteNonQuery();
+
+            Desconectar();
+            MessageBox.Show("Se Elimino correctamente el alumno");
+            clearControls();
+            CargarDataTable();
+            ResetearForm();
+        }
+    }
+}
