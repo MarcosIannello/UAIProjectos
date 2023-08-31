48ef1048399c38329bbca3e96802151904636d72
Marcos <iannellomarcos@gmail.com>
2023-08-31 08:54:04 -0300

    comienzo proyecto en capas

---
+++ PROYECTOS_LUG/PROYECTO_LUG_COMICS/DAL/CONEXION_SQL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class ConexionSQL
+    {
+        public string _conexion;
+
+        public ConexionSQL()
+        {
+            this._conexion = "Data Source=.;Initial Catalog=FACULTAD;Integrated Security=True";
+        }
+
+        public SqlConnection Open()
+        {
+            SqlConnection conexion = new SqlConnection(_conexion);
+
+            conexion.Open();
+
+            return conexion;
+
+        }
+
+        public void Close(SqlConnection connection)
+        {
+            connection.Close();
+        }
+    }
+}
